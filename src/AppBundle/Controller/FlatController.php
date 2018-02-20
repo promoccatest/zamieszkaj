29b14434453c11a896108b00b6c4eda96e623a51
programocca <info@koduje.pl>
2018-02-20 20:57:13 +0100

    widok szczegółów oferty mieszkania, kończenie i usuwane oferty

--- src/AppBundle/Controller/FlatController.php
+++ src/AppBundle/Controller/FlatController.php
@@ -13,13 +13,17 @@ use AppBundle\EventDispatcher\Events;
 use AppBundle\Entity\Flat;
 use AppBundle\Form\FlatType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class FlatController extends Controller {
     /**
+     * Metoda domyślnej listy mieszkań.
+     *
      * @Route("/mieszkania", name="flat_index")
      * @return Response
      */
@@ -38,6 +42,8 @@ class FlatController extends Controller {
     }
 
     /**
+     * Metoda dodawania mieszkania.
+     *
      * @Route("/mieszkanie/dodaj", name="flat_add")
      *
      * @param Request $request
@@ -59,7 +65,7 @@ class FlatController extends Controller {
 
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($flat);
-                $entityManager->flush();//to zapisuje do bazy
+                $entityManager->flush();
 
                 //rzucanie eventem
                 $this->get("event_dispatcher")->dispatch(Events::FLAT_ADD, new FlatEvent($flat));
@@ -74,5 +80,92 @@ class FlatController extends Controller {
         }
 
         return $this->render("UserFlat/add.html.twig", ["form" => $form->createView(), "breadcrumb" => "Dodaj mieszkanie", "content" => ""]);
+    }
+
+    /**
+     * Metoda dla widoku szczegółów mieszkań (tworzone formularze usuwania i kończenia).
+     *
+     * @Route("/mieszkanie/szczegoly/{id}", name="flat_details")
+     *
+     * @param Flat $flat
+     * @return Response
+     */
+    public function detailsAction(Flat $flat){
+        $this->denyAccessUnlessGranted("ROLE_USER");
+
+        $deleteForm = $this->createFormBuilder()
+            ->setAction($this->generateUrl("flat_delete", ["id" => $flat->getId()]))
+            ->setMethod(Request::METHOD_DELETE)
+            ->add("submit", SubmitType::class, ["label" => "Usuń"])
+            ->getForm();
+
+        $finishForm = $this->createFormBuilder()
+            ->setAction($this->generateUrl("flat_finish", ["id" => $flat->getId()]))
+            ->setMethod(Request::METHOD_POST)
+            ->add("submit", SubmitType::class, ["label" => "Zakończ"])
+            ->getForm();
+
+        return $this->render("Flat/details.html.twig",
+            [
+                "flat" => $flat,
+                "deleteForm" => $deleteForm->createView(),
+                "finishForm" => $finishForm->createView(),
+                "breadcrumb" => "Szczegóły mieszkania",
+                "content" => ""
+            ]
+        );
+    }
+
+    /**
+     * @Route("/mieszkanie/usun/{id}", name="flat_delete", methods={"DELETE"})
+     *
+     * @param Flat $flat
+     * @throws AccessDeniedException
+     * @return RedirectResponse
+     */
+    public function deleteAction(Flat $flat){
+        $this->denyAccessUnlessGranted("ROLE_USER");
+
+        if ($this->getUser() !== $flat->getOwner()) {
+            throw new AccessDeniedException();
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($flat);
+        $entityManager->flush();
+
+        $this->get("event_dispatcher")->dispatch(Events::FLAT_DELETE, new FlatEvent($flat));
+
+        $this->addFlash("success", "Mieszkanie {$flat->getTitle()} zostało usunięte.");
+
+        return $this->redirectToRoute("flat_index");
+    }
+
+    /**
+     * @Route("/mieszkanie/zakoncz/{id}", name="flat_finish", methods={"POST"})
+     *
+     * @param Flat $flat
+     * @throws AccessDeniedException
+     * @return RedirectResponse
+     */
+    public function finishAction(Flat $flat){
+        $this->denyAccessUnlessGranted("ROLE_USER");
+
+        if ($this->getUser() !== $flat->getOwner()) {
+            throw new AccessDeniedException();
+        }
+
+        $flat
+            ->setExpiresAt(new \DateTime());
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($flat);
+        $entityManager->flush();
+
+        $this->get("event_dispatcher")->dispatch(Events::FLAT_FINISH, new FlatEvent($flat));
+
+        $this->addFlash("success", "Oferta na mieszkanie {$flat->getTitle()} została zakończona.");
+
+        return $this->redirectToRoute("flat_details", ["id" => $flat->getId()]);
     }
 }
